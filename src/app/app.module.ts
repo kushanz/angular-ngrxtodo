f9947a0f98e37269d997ea438565603489aa39b9
kushanz <kushanshanuka@yahoo.com>
2021-02-16 19:54:12 +0530

    2 files modified, 4 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,11 @@ import { FormsModule } from "@angular/forms";
 import { AppComponent } from "./app.component";
 
 import { TodoContainerComponent } from "./todo-container/todo-container.component";
+import { InputTodoComponent } from './input-todo/input-todo.component';
 
 @NgModule({
   imports: [BrowserModule, FormsModule],
-  declarations: [AppComponent, TodoContainerComponent],
+  declarations: [AppComponent, TodoContainerComponent, InputTodoComponent],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
