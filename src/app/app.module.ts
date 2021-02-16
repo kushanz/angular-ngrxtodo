4b21f7945952e8a0323e98e71736c6fb2e07711c
kushanz <kushanshanuka@yahoo.com>
2021-02-16 19:35:30 +0530

    2 files modified, 1 deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,14 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { FormsModule } from "@angular/forms";
 
-import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
-import { TodoContainerComponent } from './todo-container/todo-container.component';
+import { AppComponent } from "./app.component";
+
+import { TodoContainerComponent } from "./todo-container/todo-container.component";
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent, TodoContainerComponent ],
-  bootstrap:    [ AppComponent ]
+  imports: [BrowserModule, FormsModule],
+  declarations: [AppComponent, TodoContainerComponent],
+  bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
