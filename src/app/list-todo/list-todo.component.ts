891b4306a0e1a3d886f8a14cc196db420a10fedb
kushanz <kushanshanuka@yahoo.com>
2021-02-17 17:45:49 +0530

    2 files modified

--- src/app/list-todo/list-todo.component.ts
+++ src/app/list-todo/list-todo.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { Observable } from "rxjs";
+import { Todo } from "../../store/todo.interfaces";
+import { Store } from "@ngrx/store";
 
 @Component({
-  selector: 'app-list-todo',
-  templateUrl: './list-todo.component.html',
-  styleUrls: ['./list-todo.component.css']
+  selector: "app-list-todo",
+  templateUrl: "./list-todo.component.html",
+  styleUrls: ["./list-todo.component.css"]
 })
 export class ListTodoComponent implements OnInit {
-
-  constructor() { }
+  list$: Observable<Todo[]>;
+  constructor(private store: Store<{ todoState: Array<Todo> }>) {}
 
   ngOnInit() {
+    this.list$ = this.store.select(state => state.todoState);
   }
-
 }
